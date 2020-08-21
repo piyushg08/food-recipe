d241336fd575dd46b4a7c7ba86dea8d7ba6f5bbd
piyush <piyushg296@gmail.com>
2020-08-21 17:44:35 +0530

    food-recipe

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React,{useState, useEffect} from 'react';
+import Header from './components/Header';
+import Recipes from './components/Recipes'
+import './App.css';
+import Axios from 'axios'
+
+function App() {
+
+  const [search,setSearch]=useState("chiken");
+  const [recipes,setRecipe]=useState([]);
+  const APP_ID='4757ed83';
+  const APP_KEY="54c1b53669862f4a4e51d05598ff8f0c"
+
+  useEffect(()=>{
+    getRecipes();
+  }
+    ,[])
+
+  const getRecipes= async()=>{
+    const result =await Axios.get(
+      `https://api.edamam.com/search?q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`
+      );
+      
+      setRecipe(result.data.hits);
+      console.log(result);  
+    }
+
+  const onInputChange=(e)=>{
+   setSearch(e.target.value);
+  }
+  const onSearchClick=()=>{
+    getRecipes();
+  }
+  return (
+    <div className="App">
+    
+    <Header search={search} 
+    onInputChange={onInputChange}
+    onSearchClick={onSearchClick}/>
+    
+    
+    <div className='container'>
+    <Recipes recipes={recipes} />
+    </div>
+
+     <footer>Copyright &copy;2020 | Created By Piyush</footer>
+    </div>
+
+);
+}
+
+export default App;
