8a5bef5edb45b753a3b0df3f53487365bff4166a
piyush <piyushg296@gmail.com>
2020-08-21 17:58:24 +0530

    Updates

---
+++ precache-manifest.fa8439d5e48cb1e9ac727d6d307dd546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a79a7328253dacb905a02d679295f417",
+    "url": "/food-recipe/index.html"
+  },
+  {
+    "revision": "b443a88fdc574dd83d7f",
+    "url": "/food-recipe/static/css/main.c4b18788.chunk.css"
+  },
+  {
+    "revision": "eaec164013a2b4f88e3b",
+    "url": "/food-recipe/static/js/2.d8ac1937.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-recipe/static/js/2.d8ac1937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b443a88fdc574dd83d7f",
+    "url": "/food-recipe/static/js/main.c120710a.chunk.js"
+  },
+  {
+    "revision": "779335723206f8e70329",
+    "url": "/food-recipe/static/js/runtime-main.3dc38f30.js"
+  }
+]);
